faac121cf796262c85365a78130556250f81cd9a
Dmitry Boiadji <dmitry.boiadji@gmail.com>
2022-08-13 00:57:16 +0300

    Initial Commit

---
+++ Shared/HomeTemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTemperatureApp.swift
+//  Shared
+//
+//  Created by Dmitry Boiadji on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeTemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
